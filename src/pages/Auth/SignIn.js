71c4fea11f4333ac3d977eff1390812d912d627c
hakyung <hakyung.dev@gmail.com>
2020-10-04 02:59:28 +0900

    Implement auth page

---
+++ src/pages/Auth/SignIn.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { signIn } from '../../api';
+
+const SignIn = () => {
+  const [values, setValues] = useState({});
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => setError(''), 2000);
+  }, [error]);
+
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = values;
+    if (!email || !password) {
+      return setError('모든 항목을 입력해 주세요.');
+    }
+    if (!emailRegex.test(email)) {
+      return setError('메일 형식이 유효하지 않습니다.');
+    }
+
+    const user = {
+      email,
+      password,
+    };
+
+    const result = await signIn(user);
+
+    if (result.status === 200) {
+      localStorage.setItem('token', result.data.token);
+    } else {
+      setError(result.data.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container-wide bg-signin side-left">
+      <section className="wrap-column">
+        <div className="page-top">
+          <h1 className="auth-title">Sign In</h1>
+          <div className="page-description">Welcome Back!</div>
+        </div>
+        <form className="wrap-column container-auth" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+          />
+          <div className="error">{error}</div>
+          <button className="button submit" type="submit">
+            Sign In
+          </button>
+        </form>
+        <div>
+          <Link to={`/signup`} className="link-strong">
+            Sign Up
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/Auth/SignUp.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { signUp } from '../../api';
+
+const SignUp = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [values, setValues] = useState({});
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => setError(''), 2000);
+  }, [error]);
+
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const nameRegex = /^[가-힣a-zA-Z0-9]{2,10}$/;
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, name, password, passwordConfirm } = values;
+
+    if (!email || !name || !password || !passwordConfirm) {
+      return setError('모든 항목을 입력해주세요.');
+    }
+    if (password !== passwordConfirm) {
+      return setError('비밀번호가 일치하지 않습니다.');
+    }
+    if (!emailRegex.test(email)) {
+      return setError('이메일 형식이 유효하지 않습니다.');
+    }
+    if (!passwordRegex.test(password)) {
+      return setError(
+        '비밀번호는 8자리 이상, 숫자와 문자가 1자리 이상 포함되어야 합니다.'
+      );
+    }
+    if (!nameRegex.test(name)) {
+      return setError('이름은 최소 2자리 최대 10자리 가능합니다.');
+    }
+
+    const user = {
+      email,
+      name,
+      password,
+    };
+
+    const result = await signUp(user);
+
+    if (result.status === 201) {
+      setIsSuccess(true);
+    } else {
+      setError(result.data.message);
+      setValues({});
+    }
+  };
+
+  if (isSuccess) {
+    return <Redirect to="/signin" />;
+  }
+
+  return (
+    <div className="container-wide bg-signup side-right">
+      <section className="wrap-column">
+        <div className="page-top">
+          <h1 className="auth-title">Sign Up</h1>
+          <div className="page-description">
+            And manage all your Streaming Services!
+          </div>
+        </div>
+        <form className="wrap-column container-auth" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="passwordConfirm"
+            placeholder="password confirm"
+            onChange={handleChange}
+          />
+          <div className="error">{error}</div>
+          <button className="button submit" type="submit">
+            Sign Up
+          </button>
+        </form>
+        <div>
+          Already have an account?{' '}
+          <span>
+            <Link to={`/signin`} className="link-strong">
+              Sign In
+            </Link>
+          </span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignUp;
