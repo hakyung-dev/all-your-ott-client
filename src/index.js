5e9e6d22c965f07ffda99ce4b0c866612831ef84
hakyung <hakyung.dev@gmail.com>
2020-11-09 02:08:00 +0900

    Fix Router to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
