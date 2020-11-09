931908c2622bd8dc84ae6da0cfe678806dd1e906
hakyung <hakyung.dev@gmail.com>
2020-11-10 03:38:17 +0900

    Test server

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://ayoapi.devhak.com';
+const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://ayoserver-env-1.eba-pmjh2j3t.ap-northeast-2.elasticbeanstalk.com/';
 
 module.exports = { SERVER_URL };
