06928d13cce7da348e817b32cadbbca5c78d2e1a
hakyung <hakyung.dev@gmail.com>
2020-12-01 10:25:03 +0900

    Change server domain

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://ayoapi.devhak.com/';
+const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://ayoapi.hakyung.site/';
 
 module.exports = { SERVER_URL };
