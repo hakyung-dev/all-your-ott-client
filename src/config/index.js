f972f57a83b59a4aebf988cea4e7a41c1eadc6ee
hakyung <hakyung.dev@gmail.com>
2021-05-19 14:30:42 +0900

    Renew server url

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://ayoapi.hakyung.site/';
+const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://ayoapi.devhak.xyz/';
 
 module.exports = { SERVER_URL };
